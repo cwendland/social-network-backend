d1daad3857519275894574406c3e11c94e514c96
cwendland <chris.r.wendland@gmail.com>
2022-10-27 20:44:56 -0700

    Added user controllers

--- routes/api/thoughRoutes.js
+++ routes/api/thoughRoutes.js
@@ -3,4 +3,22 @@ const {
     getThoughts,
     getSingleThought,
     createThought,
+    updateThought,
+    deleteThought,
+    addResponse,
+    removeResponse,
 } = require('../../controllers/thoughtController');
+
+router.route('/').get(getThoughts).post(createThought);
+
+router
+    .route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+router.route('/:thoughtId/responses').post(addResponse);
+
+router.route('/:thoughtId/responses/:responseId').delete(removeResponse);
+
+module.exports = router;
